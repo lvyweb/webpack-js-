d59f7558b4e4feea7095bf825832cb2d9a799e80
liangyuanyuan <lvyweb@163.com>
2018-03-29 16:15:15 +0800

    提交

---
+++ dist/resource/js/index.bundle.js
@@ -0,0 +1,21 @@
+webpackJsonp([1],{
+
+/***/ 27:
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function($) {
+__webpack_require__(28);
+console.log('进入indexjs了=');
+console.log($('h1').html() + "我是index.js输出的");
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ }),
+
+/***/ 28:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},[27]);
---
+++ dist/resource/js/register.bundle.js
@@ -0,0 +1,19 @@
+webpackJsonp([2],{
+
+/***/ 29:
+/***/ (function(module, exports, __webpack_require__) {
+
+/* WEBPACK VAR INJECTION */(function($) {var registerjs = {
+  sayHello: function () {
+    console.log('register.js');
+  }
+
+};
+console.log('进入registerjs了');
+console.log($('h1').html() + "我是register.js输出的");
+module.exports = registerjs;
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ })
+
+},[29]);
